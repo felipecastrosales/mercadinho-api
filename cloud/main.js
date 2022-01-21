538ec7db1bb067f0edd87ab6a7a124ca2d596745
Felipe Sales <soufeliposales@gmail.com>
2022-01-21 13:05:01 -0300

    paginating products page

--- cloud/main.js
+++ cloud/main.js
@@ -6,7 +6,11 @@ Parse.Cloud.define('hello', (request) => {
 
 Parse.Cloud.define('get-product-list', async (request) => {
     const queryProducts = new Parse.Query(Product);
-	// Conditionals 
+	// Conditionals
+	const itemsPerPage = request.params.itemsPerPage || 20;
+	if (itemsPerPage > 100) throw 'Invalid quantity of items per page';
+	queryProducts.skip(itemsPerPage * request.params.page || 0);
+	queryProducts.limit(itemsPerPage);
 	const resultProducts = await queryProducts.find({useMasterKey: true});
 	return resultProducts.map(function(p){
 		p = p.toJSON();
@@ -17,6 +21,6 @@ Parse.Cloud.define('get-product-list', async (request) => {
 			price: p.price,
 			unit: p.unit,
 			picture: p.picture != null ? p.picture.url : null,
-		} 
+		}
 	});
 });
