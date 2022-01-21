24202fee24ab78bb98fc51f6c63ece451b056295
Felipe Sales <soufeliposales@gmail.com>
2022-01-21 11:35:15 -0300

    returning products endpoints

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,22 @@
-Parse.Cloud.define("hello", (request) => {
-	return "Hello world from Mercadinho!";
+const Product = Parse.Object.extend('Product');
+
+Parse.Cloud.define('hello', (request) => {
+	return 'Hello world from Mercadinho!';
+});
+
+Parse.Cloud.define('get-product-list', async (request) => {
+    const queryProducts = new Parse.Query(Product);
+	// Conditionals 
+	const resultProducts = await queryProducts.find({useMasterKey: true});
+	return resultProducts.map(function(p){
+		p = p.toJSON();
+		return {
+			id: p.objectId,
+			title: p.title,
+			description: p.description,
+			price: p.price,
+			unit: p.unit,
+			picture: p.picture != null ? p.picture.url : null,
+		} 
+	});
 });
