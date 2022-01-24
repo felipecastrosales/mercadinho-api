4acee54b3b068ed297b8d9b0e522812edb9febdf
Felipe Sales <soufeliposales@gmail.com>
2022-01-24 11:22:45 -0300

    modifying item quantity

--- cloud/main.js
+++ cloud/main.js
@@ -121,6 +121,18 @@ Parse.Cloud.define('add-item-to-cart', async (request) => {
 	}
 });
 
+Parse.Cloud.define('modify-item-quantity', async (request) => {
+	if (request.params.cartItemId == null) throw 'INVALID_CART_ITEM';
+	if (request.params.quantity == null) throw 'INVALID_QUANTITY';
+	const cartItem = new CartItem();
+	cartItem.id = request.params.cartItemId;
+    if (request.params.quantity > 0) {
+		cartItem.set('quantity', request.params.quantity);
+		await cartItem.save(null, {useMasterKey: true});
+	} else {
+		await cartItem.destroy({useMasterKey: true});
+	}
+});
 
 function formatUser(userJson) {
 	return {
